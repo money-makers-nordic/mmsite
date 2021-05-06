9d5f0bb12b3d47e6c222a7ac6d8785457830edaf
isakcodes <61279302+isakcodes@users.noreply.github.com>
2021-05-06 11:13:35 +0200

    changed edit paths again

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,13 +83,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.json'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/money-makers-nordic/money-makers-nordic.github.io/edit/master/website/',
+            'https://github.com/money-makers-nordic/money-makers-nordic.github.io/edit/master/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/money-makers-nordic/money-makers-nordic.github.io/edit/master/website/blog/',
+            'https://github.com/money-makers-nordic/money-makers-nordic.github.io/edit/master/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
